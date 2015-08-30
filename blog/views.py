1812c03b9b9197959d42cdd0753ce100e0ecef1a
marth <marthaurion@gmail.com>
2015-08-29 21:19:51 -0500

    implemented first view to list posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from .models import Post
 
 # Create your views here.
+def post_index(request):
+    return render_to_response('blog/post_index.html',
+                               { 'post_list': Post.objects.all() })
