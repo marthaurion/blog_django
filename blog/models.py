ff454ef29583a03c819c9d127ac4a8468300fd26
marth <marthaurion@gmail.com>
2015-08-30 21:31:29 -0500

    added some more robust category tracking and added a filter to only display posts published before today

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,20 @@
 from django.db import models
-import datetime
+from django.utils import timezone
 
 # Create your models here.
+class PostManager(models.Manager):
+    def get_queryset(self):
+        return super(PostManager, self).get_queryset().filter(pub_date__lte=timezone.now)
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique_for_date='pub_date')
     excerpt = models.CharField(max_length=200)
     body = models.TextField()
     category = models.ForeignKey('Category')
-    pub_date = models.DateTimeField('date published', default=datetime.datetime.now)
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+    objects = models.Manager()
+    published = PostManager()
     
     class Meta:
         ordering = ['-pub_date']
@@ -30,6 +36,16 @@ class Category(models.Model):
         
     def __str__(self):
         return self.title
-        
+    
+    def get_descendants(self):
+        children = []
+        for child in self.category_set.all():
+            children.append(child)
+            temp = child.get_descendants()
+            if len(temp) > 0:
+                children.append(temp)
+            
+        return children
+    
     def get_absolute_url(self):
         return "/blog/category/%s/" % self.slug
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post, Category
 
 # Create your views here.
 def post_index(request, page=1):
-    posts = Post.objects.all()
+    posts = Post.published.all()
     paginator = Paginator(posts, 1)
     
     page = int(page)
@@ -18,14 +18,10 @@ def post_index(request, page=1):
                                  'categories': Category.objects.filter(parent__isnull=True) })
 
 def category_index(request, slug, page=1):
-    category_list = []
     category = Category.objects.get(slug=slug)
+    category_list = category.get_descendants()
     category_list.append(category)
-    
-    for child in category.category_set.all():
-        category_list.append(child)
-    
-    posts = Post.objects.filter(category__in=category_list)
+    posts = Post.published.filter(category__in=category_list)
     
     paginator = Paginator(posts, 1)
     
@@ -41,12 +37,15 @@ def category_index(request, slug, page=1):
 
 def post_detail(request, year, month, day, slug):
     import datetime, time
+    from django.utils import timezone
     date_stamp = time.strptime(year+month+day, "%Y%m%d")
     pub_date = datetime.date(*date_stamp[:3])
     post = get_object_or_404(Post, pub_date__year=pub_date.year,
                                    pub_date__month=pub_date.month,
                                    pub_date__day=pub_date.day,
                                    slug=slug)
+    if post.pub_date>timezone.now():
+        raise Http404()
     return render_to_response('blog/post_detail.html',
                                 { 'post': post,
                                   'categories': Category.objects.filter(parent__isnull=True) })
