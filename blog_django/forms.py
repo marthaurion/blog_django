116aa1b4f37dd0a9e8b995130c15cd53c9003cb0
Marth <marthaurion@gmail.com>
2016-05-22 18:42:50 +0000

    messed with the contact form a bit to make everything required

--- blog_django/forms.py
+++ blog_django/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    sender = forms.EmailField()
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
+    sender = forms.EmailField(label="E-mail", required=True)
+    subject = forms.CharField(max_length=100, required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
