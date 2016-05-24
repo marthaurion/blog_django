15989a815ef44978094e5cdb33724bfb2ea0d20e
Marth <marthaurion@gmail.com>
2016-05-24 16:05:42 +0000

    switch to using cloudfront to serve the static files instead of pulling directly from S3

--- blog_django/custom_storages.py
+++ blog_django/custom_storages.py
@@ -4,5 +4,13 @@ from storages.backends.s3boto import S3BotoStorage
 class StaticStorage(S3BotoStorage):
     location = settings.STATICFILES_LOCATION
     
+    def __init__(self, *args, **kwargs):
+        kwargs['custom_domain'] = settings.AWS_CLOUDFRONT_DOMAIN
+        super(StaticStorage, self).__init__(*args, **kwargs)
+    
 class MediaStorage(S3BotoStorage):
     location = settings.MEDIAFILES_LOCATION
+    
+    def __init__(self, *args, **kwargs):
+        kwargs['custom_domain'] = settings.AWS_CLOUDFRONT_DOMAIN
+        super(MediaStorage, self).__init__(*args, **kwargs)
--- blog_django/settings/base.py
+++ blog_django/settings/base.py
@@ -100,12 +100,13 @@ USE_TZ = True
 AWS_STORAGE_BUCKET_NAME = 'marthaurion'
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_CLOUDFRONT_DOMAIN = "dbr635qdrhpoe.cloudfront.net"
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'blog_django.custom_storages.StaticStorage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+STATIC_URL = "https://%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, STATICFILES_LOCATION)
 
 MEDIAFILES_LOCATION = 'media'
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+MEDIA_URL = "https://%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = 'blog_django.custom_storages.MediaStorage'
 
 EMAIL_USE_TLS = True
