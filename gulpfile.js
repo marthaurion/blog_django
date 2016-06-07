7ac2635604f1ed08362ece9bed05a974448cafad
Marth <marthaurion@gmail.com>
2016-06-07 14:30:23 +0000

    created a new combined CSS file to hold all CSS information with gulp and added material kit theme

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
+var concatCSS = require('gulp-concat-css');
+var cleanCSS = require('gulp-clean-css');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default',function() {
+    return gulp.src(['assets/css/bootstrap.min.css', 'assets/css/material-kit.css', 'assets/css/mainstuff.css'])
+        .pipe(concatCSS('css/allv1.css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('assets'));
 });
