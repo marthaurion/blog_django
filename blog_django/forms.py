bcf82e26350230b7ea7b931c66e93509573728a9
Marth <marthaurion@gmail.com>
2016-06-10 02:19:33 +0000

    various visual updates. changed the way tags and posts are displayed. made the contact form look better

--- blog_django/forms.py
+++ blog_django/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    sender = forms.EmailField(label="E-mail", required=True)
-    subject = forms.CharField(max_length=100, required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    sender = forms.EmailField(label="E-mail", required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    subject = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), required=True)
