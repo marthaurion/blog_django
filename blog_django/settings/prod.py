30001cc870aaf87c846c47f99732ee4001764121
Marth <marthaurion@gmail.com>
2016-06-18 20:13:50 +0000

    switch from https to http in cloudfront url

--- blog_django/settings/prod.py
+++ blog_django/settings/prod.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = False
 
 STATICFILES_STORAGE = 'blog_django.custom_storages.StaticStorage'
-STATIC_URL = "https://%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, STATICFILES_LOCATION)
+STATIC_URL = "http://%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, STATICFILES_LOCATION)
 
-MEDIA_URL = "https://%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, MEDIAFILES_LOCATION)
+MEDIA_URL = "http://%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = 'blog_django.custom_storages.MediaStorage'
