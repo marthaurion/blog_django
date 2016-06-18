0ca8548ac5a1fd19ea008f3445de19db0d4b96b4
Marth <marthaurion@gmail.com>
2016-06-18 20:20:37 +0000

    move aws stuff to prod settings files

--- blog_django/settings/prod.py
+++ blog_django/settings/prod.py
@@ -6,8 +6,10 @@ DEBUG = False
 AWS_CLOUDFRONT_DOMAIN = "d34zzkuru6phz2.cloudfront.net"
 #AWS_CLOUDFRONT_DOMAIN = "media.codebecauseican.com"
 
-STATICFILES_STORAGE = 'blog_django.custom_storages.StaticStorage'
+STATICFILES_LOCATION = 'static'
 STATIC_URL = "https://%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, STATICFILES_LOCATION)
+STATICFILES_STORAGE = 'blog_django.custom_storages.StaticStorage'
 
+MEDIAFILES_LOCATION = 'media'
 MEDIA_URL = "https://%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = 'blog_django.custom_storages.MediaStorage'
