4f38d9c7e51e3ecbb52dfc664d786a6d4b542675
Marth <marthaurion@gmail.com>
2016-06-21 13:28:25 +0000

    change back

--- blog_django/settings/prod.py
+++ blog_django/settings/prod.py
@@ -3,15 +3,15 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-#AWS_CLOUDFRONT_DOMAIN = "d34zzkuru6phz2.cloudfront.net"
-AWS_CLOUDFRONT_DOMAIN = "media.codebecauseican.com"
+AWS_CLOUDFRONT_DOMAIN = "d34zzkuru6phz2.cloudfront.net"
+#AWS_CLOUDFRONT_DOMAIN = "media.codebecauseican.com"
 
 STATICFILES_LOCATION = 'static'
-#STATIC_URL = "https://%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, STATICFILES_LOCATION)
-STATIC_URL = "//%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, STATICFILES_LOCATION)
+STATIC_URL = "https://%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, STATICFILES_LOCATION)
+#STATIC_URL = "//%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, STATICFILES_LOCATION)
 STATICFILES_STORAGE = 'blog_django.custom_storages.StaticStorage'
 
 MEDIAFILES_LOCATION = 'media'
-#MEDIA_URL = "https://%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, MEDIAFILES_LOCATION)
-MEDIA_URL = "//%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, MEDIAFILES_LOCATION)
+MEDIA_URL = "https://%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, MEDIAFILES_LOCATION)
+#MEDIA_URL = "//%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = 'blog_django.custom_storages.MediaStorage'
