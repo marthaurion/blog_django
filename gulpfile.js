b13f28b3069e7ddb4fcc5cbbd8867afe52922701
Marth <marthaurion@gmail.com>
2016-07-14 12:10:48 +0000

    add theme switch

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,18 @@ var gulp = require('gulp');
 var concatCSS = require('gulp-concat-css');
 var cleanCSS = require('gulp-clean-css');
 
-gulp.task('default',function() {
+gulp.task('dark',function() {
     return gulp.src(['assets/css/darkly.css', 'assets/css/mainstuff.css'])
-        .pipe(concatCSS('css/allv5.css'))
+        .pipe(concatCSS('css/dark1.css'))
         .pipe(cleanCSS())
         .pipe(gulp.dest('assets'));
 });
+
+gulp.task('light',function() {
+    return gulp.src(['assets/css/flatly.css', 'assets/css/mainstuff.css'])
+        .pipe(concatCSS('css/light1.css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('assets'));
+});
+
+gulp.task('default', ['dark', 'light']);
