7cd49ac203ef3efe792e6777d70c606e13dcbf52
Marth <marthaurion@gmail.com>
2016-07-14 02:59:58 +0000

    add fonts

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ var concatCSS = require('gulp-concat-css');
 var cleanCSS = require('gulp-clean-css');
 
 gulp.task('default',function() {
-    return gulp.src(['assets/css/darkly.min.css', 'assets/css/mainstuff.css'])
-        .pipe(concatCSS('css/allv4.css'))
+    return gulp.src(['assets/css/darkly.css', 'assets/css/mainstuff.css'])
+        .pipe(concatCSS('css/allv5.css'))
         .pipe(cleanCSS())
         .pipe(gulp.dest('assets'));
 });
