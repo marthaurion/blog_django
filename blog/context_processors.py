71632792a7ccc62bb9232cc5b76cc3d9a69cf2d3
Marth <marthaurion@gmail.com>
2017-03-30 15:18:06 +0000

    initial implementation of recent comments

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,4 +1,8 @@
-from .models import Category
+from .models import Category, Comment
 
 def category_list(request):
-    return {'categories': Category.objects.all()} 
+    return { 'categories': Category.objects.all() }
+    
+def recent_comments(request):
+    comment_set = Comment.objects.filter(approved=True, post__isnull=False).select_related('post', 'author').order_by('-pub_date')[:5]
+    return { 'recent_comments': comment_set }
--- blog_django/settings/base.py
+++ blog_django/settings/base.py
@@ -91,6 +91,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'blog.context_processors.category_list',
+                'blog.context_processors.recent_comments',
             ],
             'loaders': [
                 ('django.template.loaders.cached.Loader', [
