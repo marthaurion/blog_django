e7a87a69bd13852ae8c76779855044688a7c6df8
Marth <marthaurion@gmail.com>
2017-04-01 16:17:52 +0000

    fix the collapsing categories since we no longer have jquery

--- blog/models.py
+++ blog/models.py
@@ -156,12 +156,12 @@ class Category(MPTTModel):
         if self.is_leaf_node():
             prefix = '&#8212; &nbsp;'
         else:
-            prefix = '<a class="collapsible" href="#%s" data-toggle="collapse">%s</a> &nbsp;' % (self.pk, prefix_character)
+            prefix = '<a class="collapsible" href="#%s">%s</a> &nbsp;' % (self.pk, prefix_character)
         return '<li>%s<a href="%s">%s</a></li>\n' % (prefix, self.get_absolute_url(), self.title)
     
     def get_active_string(self):
         if self.active:
-            return " in"
+            return "in"
         return ""
         
 class Media(models.Model):
