b860a617785e7e3258ccc142cde1d6619bd9a15d
Marth <marthaurion@gmail.com>
2017-07-04 16:22:08 +0000

    storages

--- blog_django/custom_storages.py
+++ blog_django/custom_storages.py
@@ -1,15 +1,15 @@
 from django.conf import settings
 
-from storages.backends.s3boto3 import S3BotoStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
-class StaticStorage(S3BotoStorage):
+class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
     
     def __init__(self, *args, **kwargs):
         kwargs['custom_domain'] = settings.AWS_CLOUDFRONT_DOMAIN
         super().__init__(*args, **kwargs)
     
-class MediaStorage(S3BotoStorage):
+class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
     
     def __init__(self, *args, **kwargs):
