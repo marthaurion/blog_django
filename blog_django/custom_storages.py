2052e2c44ac1fa80a0146cb0c0e4542b6b43ef17
Marth <marthaurion@gmail.com>
2017-07-04 16:14:32 +0000

    boto 3

--- blog_django/custom_storages.py
+++ blog_django/custom_storages.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto3 import S3BotoStorage
 
 class StaticStorage(S3BotoStorage):
     location = settings.STATICFILES_LOCATION
